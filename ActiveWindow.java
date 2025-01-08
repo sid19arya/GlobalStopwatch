ccd3370a84b208660b1790d7f5b68d10f4589435
sid <sid19arya@gmail.com>
2025-01-08 01:13:27 -0500

    Configure use of JNI in order to grab active window info from OS and corresponding C source file

---
+++ ActiveWindow.java
@@ -0,0 +1,19 @@
+public class ActiveWindow {
+    // Load the native library
+    static {
+        System.loadLibrary("ActiveWindow");
+    }
+
+    // Declare the native method
+    public native String getActiveWindowTitle();
+
+    public static void main(String[] args) throws InterruptedException {
+        ActiveWindow aw = new ActiveWindow();
+
+        while (true) {
+            String title = aw.getActiveWindowTitle();
+            System.out.println("Active Window: " + title);
+            Thread.sleep(1000);
+        }
+    }
+}
